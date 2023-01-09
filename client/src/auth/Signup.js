faa1fc9ebb8d9c81ec3530612e4257e1de8dbc56
Andrew <44222520+andrewjthomsen@users.noreply.github.com>
2023-01-09 10:29:42 -0800

    Modified controllers auth file to replace sendgrid with nodemailer

--- client/src/auth/Signup.js
+++ client/src/auth/Signup.js
@@ -1,48 +1,62 @@
-import React, {useState} from 'react'
-import {Link, Redirect} from 'react-router-dom'
-import Layout from '../core/Layout'
-import axios from 'axios'
-import {ToastContainer, toast} from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.min.css'
+
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import Layout from '../core/Layout';
+import axios from 'axios';
+// import { isAuth } from './helpers';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 const Signup = () => {
     const [values, setValues] = useState({
-        name: 'test',
-        email: 'dd@dummy.com',
-        password: 'password1',
+        name: 'Ryan',
+        email: 'kaloraatjs@gmail.com',
+        password: 'rrrrrr',
         buttonText: 'Submit'
     });
 
-    const {name, email, password, buttonText} = values;
-    const handleChange = (name) => (event) => {
-        console.log(event.target.value);
-        setValues({...values, [name]: event.target.value});
+    const { name, email, password, buttonText } = values;
+
+    const handleChange = name => event => {
+        // console.log(event.target.value);
+        setValues({ ...values, [name]: event.target.value });
     };
 
     const clickSubmit = event => {
-        //
-        event.preventDefault()
-        setValues({...values, buttonText: 'Submitting'})
+        event.preventDefault();
+        setValues({ ...values, buttonText: 'Submitting' });
         axios({
-            method: ''
+            method: 'POST',
+            url: `${process.env.REACT_APP_API}/signup`,
+            data: { name, email, password }
         })
-    }
+            .then(response => {
+                console.log('SIGNUP SUCCESS', response);
+                setValues({ ...values, name: '', email: '', password: '', buttonText: 'Submitted' });
+                toast.success(response.data.message);
+            })
+            .catch(error => {
+                console.log('SIGNUP ERROR', error.response.data);
+                setValues({ ...values, buttonText: 'Submit' });
+                toast.error(error.response.data.error);
+            });
+    };
 
     const signupForm = () => (
         <form>
             <div className="form-group">
                 <label className="text-muted">Name</label>
-                <input onChange={handleChange('name')} value={name} type="text" className="form-control"/>
+                <input onChange={handleChange('name')} value={name} type="text" className="form-control" />
             </div>
 
             <div className="form-group">
                 <label className="text-muted">Email</label>
-                <input onChange={handleChange('email')} value={email} type="email" className="form-control"/>
+                <input onChange={handleChange('email')} value={email} type="email" className="form-control" />
             </div>
 
             <div className="form-group">
                 <label className="text-muted">Password</label>
-                <input onChange={handleChange('password')} value={password} type="password" className="form-control"/>
+                <input onChange={handleChange('password')} value={password} type="password" className="form-control" />
             </div>
 
             <div>
@@ -56,8 +70,7 @@ const Signup = () => {
     return (
         <Layout>
             <div className="col-md-6 offset-md-3">
-                <ToastContainer/>
-                {JSON.stringify({name, email, password })}
+                <ToastContainer />
                 <h1 className="p-5 text-center">Signup</h1>
                 {signupForm()}
             </div>
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,11 +1,46 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
-// sendgrid
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const { sendEmailWithNodemailer } = require("../helpers/email");
+ 
+exports.signup = (req, res) => {
+  const { name, email, password } = req.body;
+ 
+  User.findOne({ email }).exec((err, user) => {
+    if (user) {
+      return res.status(400).json({
+        error: "Email is taken",
+      });
+    }
+ 
+    const token = jwt.sign(
+      { name, email, password },
+      process.env.JWT_ACCOUNT_ACTIVATION,
+      { expiresIn: "10m" }
+    );
+ 
+    const emailData = {
+      from: "masterjupiter2015@gmail.com", // MAKE SURE THIS EMAIL IS YOUR GMAIL FOR WHICH YOU GENERATED APP PASSWORD
+      to: email, // WHO SHOULD BE RECEIVING THIS EMAIL? IT SHOULD BE THE USER EMAIL (VALID EMAIL ADDRESS) WHO IS TRYING TO SIGNUP
+      subject: "ACCOUNT ACTIVATION LINK",
+      html: `
+                <h1>Please use the following link to activate your account</h1>
+                <p>http://localhost:3000/auth/activate/${token}</p>
+                <hr />
+                <p>This email may contain sensitive information</p>
+                <p>http://localhost:3000</p>
+            `,
+    };
+ 
+    sendEmailWithNodemailer(req, res, emailData);
+ 
+  });
+};
+// // sendgrid
+// const sgMail = require('@sendgrid/mail');
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // exports.signup = (req, res) => {
-//     // console.log('REQ BODY ON SIGNUP', req.body);
 //     const { name, email, password } = req.body;
 
 //     User.findOne({ email }).exec((err, user) => {
@@ -14,64 +49,38 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 //                 error: 'Email is taken'
 //             });
 //         }
-//     });
 
-//     let newUser = new User({ name, email, password });
+//         const token = jwt.sign({ name, email, password }, process.env.JWT_ACCOUNT_ACTIVATION, { expiresIn: '10m' });
 
-//     newUser.save((err, success) => {
-//         if (err) {
-//             console.log('SIGNUP ERROR', err);
-//             return res.status(400).json({
-//                 error: err
+//         const emailData = {
+//             from: process.env.EMAIL_FROM,
+//             to: email,
+//             subject: `Account activation link`,
+//             html: `
+//                 <h1>Please use the following link to activate your account</h1>
+//                 <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
+//                 <hr />
+//                 <p>This email may contain sensetive information</p>
+//                 <p>${process.env.CLIENT_URL}</p>
+//             `
+//         };
+
+//         sgMail
+//             .send(emailData)
+//             .then(sent => {
+//                 // console.log('SIGNUP EMAIL SENT', sent)
+//                 return res.json({
+//                     message: `Email has been sent to ${email}. Follow the instruction to activate your account`
+//                 });
+//             })
+//             .catch(err => {
+//                 // console.log('SIGNUP EMAIL SENT ERROR', err)
+//                 return res.json({
+//                     message: err.message
+//                 });
 //             });
-//         }
-//         res.json({
-//             message: 'Signup success! Please signin'
-//         });
 //     });
 // };
-
-exports.signup = (req, res) => {
-    const { name, email, password } = req.body;
-
-    User.findOne({ email }).exec((err, user) => {
-        if (user) {
-            return res.status(400).json({
-                error: 'Email is taken'
-            });
-        }
-
-        const token = jwt.sign({ name, email, password }, process.env.JWT_ACCOUNT_ACTIVATION, { expiresIn: '10m' });
-
-        const emailData = {
-            from: process.env.EMAIL_FROM,
-            to: email,
-            subject: `Account activation link`,
-            html: `
-                <h1>Please use the following link to activate your account</h1>
-                <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
-                <hr />
-                <p>This email may contain sensetive information</p>
-                <p>${process.env.CLIENT_URL}</p>
-            `
-        };
-
-        sgMail
-            .send(emailData)
-            .then(sent => {
-                // console.log('SIGNUP EMAIL SENT', sent)
-                return res.json({
-                    message: `Email has been sent to ${email}. Follow the instruction to activate your account`
-                });
-            })
-            .catch(err => {
-                // console.log('SIGNUP EMAIL SENT ERROR', err)
-                return res.json({
-                    message: err.message
-                });
-            });
-    });
-};
 
 exports.accountActivation = (req, res) => {
     const { token } = req.body;
@@ -133,3 +142,4 @@ exports.signin = (req, res) => {
         });
     });
 };
+
