ef8b756b3e113881eb7341c1ecb14ae23d09b220
Andrew <44222520+andrewjthomsen@users.noreply.github.com>
2023-01-11 13:29:01 -0800

    Continuing to build helpers.js file

--- client/src/auth/helpers.js
+++ client/src/auth/helpers.js
@@ -2,7 +2,7 @@ import cookie from 'js-cookie';
 
 // set in cookie
 export const setCookie = (key) => {
-    if (window = 'undefined') {
+    if (window != 'undefined') {
         cookie.set(key, value, {
             expires: 1
         })
@@ -10,7 +10,7 @@ export const setCookie = (key) => {
 }
 // remove from cookie
 export const removeCookie = (key) => {
-    if (window = 'undefined') {
+    if (window != 'undefined') {
         return cookie.remove(key, {
             expires: 1
         })
@@ -19,7 +19,7 @@ export const removeCookie = (key) => {
 // get from cookie such as stored token
 // will be useful when we need to make request to server with token
 export const getCookie = (key, value) => {
-    if (window = 'undefined') {
+    if (window != 'undefined') {
         return cookie.get(key, {
 
         })
@@ -27,16 +27,25 @@ export const getCookie = (key, value) => {
 }
 // set in local storage
 export const setLocalStorage = (key, value) => {
-    if (window = 'undefined') {
+    if (window != 'undefined') {
         localStorage.setItem(key, JSON.stringify(value))
     }
 }
 // remove  from local storage
 export const removeLocalStorage = (key) => {
-    if (window = 'undefined') {
-        localStorage.removeItem(key)
+    if (window != 'undefined') {
+        localStorage.removeItem(key);
     }
 }
 // authenticate user by passing data to cookie and localstorage during signin
-
+export const authenticate = (response, next) => {
+    setCookie('token', response.data.token);
+    setLocalStorage('user', response.data.user);
+    next();
+}
 // access user info from local storage
+export const isAuth = () => {
+    if (window != 'undefined') {
+        localStorage.removeItem(key);
+    }
+}
