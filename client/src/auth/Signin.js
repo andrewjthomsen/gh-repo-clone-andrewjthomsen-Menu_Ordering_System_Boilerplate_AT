ae4d8d91fa93e38aa86157dc35ab3c4d082dc2f8
Andrew <44222520+andrewjthomsen@users.noreply.github.com>
2023-01-12 15:29:09 -0800

    Addeding authenticate and signout functionality

--- client/src/auth/Signin.js
+++ client/src/auth/Signin.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import Layout from '../core/Layout';
 import axios from 'axios';
-import { authenticate, authentice } from './helpers';
+import { authenticate } from './helpers';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 
@@ -32,10 +32,9 @@ const Signin = () => {
                 console.log('SIGNIN SUCCESS', response);
                 // save the response (user, token) localstorage/cookie
                 authenticate(response, () => {
-
-                })
-                setValues({ ...values, name: '', email: '', password: '', buttonText: 'Submitted' });
-                toast.success(`Hey ${response.data.user.name}, Welcome back!`);
+                    setValues({ ...values, name: '', email: '', password: '', buttonText: 'Submitted' });
+                    toast.success(`Hey ${response.data.user.name}, Welcome back!`);
+                });
             })
             .catch(error => {
                 console.log('SIGNIN ERROR', error.response.data);
--- client/src/auth/helpers.js
+++ client/src/auth/helpers.js
@@ -2,7 +2,7 @@ import cookie from 'js-cookie';
 
 // set in cookie
 export const setCookie = (key, value) => {
-    if (window != 'undefined') {
+    if (window !== 'undefined') {
         cookie.set(key, value, {
             expires: 1
         })
@@ -10,7 +10,7 @@ export const setCookie = (key, value) => {
 }
 // remove from cookie
 export const removeCookie = key => {
-    if (window != 'undefined') {
+    if (window !== 'undefined') {
         return cookie.remove(key, {
             expires: 1
         })
@@ -19,7 +19,7 @@ export const removeCookie = key => {
 // get from cookie such as stored token
 // will be useful when we need to make request to server with token
 export const getCookie = (key) => {
-    if (window != 'undefined') {
+    if (window !== 'undefined') {
         return cookie.get(key, {
 
         })
@@ -27,13 +27,13 @@ export const getCookie = (key) => {
 }
 // set in local storage
 export const setLocalStorage = (key, value) => {
-    if (window != 'undefined') {
+    if (window !== 'undefined') {
         localStorage.setItem(key, JSON.stringify(value))
     }
 }
 // remove  from local storage
 export const removeLocalStorage = key => {
-    if (window != 'undefined') {
+    if (window !== 'undefined') {
         localStorage.removeItem(key);
     }
 }
@@ -45,7 +45,7 @@ export const authenticate = (response, next) => {
 }
 // access user info from local storage
 export const isAuth = () => {
-    if (window != 'undefined') {
+    if (window !== 'undefined') {
         const cookieChecked = getCookie('token')
         if (cookieChecked) {
             if (localStorage.getItem('user')) {
