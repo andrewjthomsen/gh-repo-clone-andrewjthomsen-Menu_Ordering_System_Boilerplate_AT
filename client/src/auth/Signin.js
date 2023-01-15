4342e467af01a83d9efc4b5a903ba5a73748a831
Andrew <44222520+andrewjthomsen@users.noreply.github.com>
2023-01-14 20:07:52 -0800

    Further built out admin and private routing functionality.

--- client/src/auth/Signin.js
+++ client/src/auth/Signin.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, Redirect} from 'react-router-dom';
 import Layout from '../core/Layout';
 import axios from 'axios';
-import { authenticate, authentice } from './helpers';
-import { ToastContainer, toast } from 'react-toastify';
+import {authenticate, isAuth} from './helpers';
+import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
@@ -11,50 +11,66 @@ import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import TakeoutDiningIcon from '@mui/icons-material/TakeoutDining';
 
-export default function Login(){
+export default function Login({history}) {
     const [values, setValues] = useState({
         email: '',
         password: '',
         buttonText: ''
     });
-    const { email, password, buttonText } = values;
+    const {email, password, buttonText} = values;
 
     const handleChange = name => event => {
         // console.log(event.target.value);
-        setValues({ ...values, [name]: event.target.value });
+        setValues({...values, [name]: event.target.value});
     };
     const clickSubmit = event => {
         console.log("Button was pressed");
         event.preventDefault();
-        setValues({ ...values, buttonText: 'Submitting' });
+        setValues({...values, buttonText: 'Submitting'});
         axios({
             method: 'POST',
             url: `${process.env.REACT_APP_API}/signin`,
-            data: { email, password }
+            data: {email, password}
         })
             .then(response => {
                 console.log('SIGNIN SUCCESS', response);
                 // save the response (user, token) localstorage/cookie
                 authenticate(response, () => {
+                    setValues({...values, name: '', email: '', password: '', buttonText: 'Submitted'});
+                    // toast.success(`Hey ${response.data.user.name}, Welcome back!`);
+                    // user will be redirected after signing either to admin
+                    // if role is admin or to private if user is a subscriber
+                    isAuth() && isAuth().role === 'admin' ? history.push('/admin') : history.push('private');
 
-                })
-                setValues({ ...values, name: '', email: '', password: '', buttonText: 'Submitted' });
-                toast.success(`Hey ${response.data.user.name}, Welcome back!`);
+                });
             })
             .catch(error => {
                 console.log('SIGNIN ERROR', error.response.data);
-                setValues({ ...values, buttonText: 'Submit' });
+                setValues({...values, buttonText: 'Submit'});
                 toast.error(error.response.data.error);
             });
     };
     return (
         <Grid container justifyContent={"center"} alignItems={"center"} sx={{height: '100vh'}}>
             <Grid item xs={1}></Grid>
-            <Grid container spacing={0} justifyContent={"center"} alignItems={"center"} sx={{height: '90vh', backgroundImage: 'url(/background.jpg)'}}>
-                <Box sx={{bgcolor: 'white', borderRadius: '16px', boxShadow: 10, border: 1, display: 'flex', flexDirection: 'column', textAlign: 'center', justifyContent: 'center', alignItems: 'center'}}>
+            <Grid container spacing={0} justifyContent={"center"} alignItems={"center"}
+                  sx={{height: '90vh', backgroundImage: 'url(/background.jpg)'}}>
+                <Box sx={{
+                    bgcolor: 'white',
+                    borderRadius: '16px',
+                    boxShadow: 10,
+                    border: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    textAlign: 'center',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
                     <TakeoutDiningIcon sx={{height: 100, width: 100, mt: 2}}/>
-                    <TextField onChange={handleChange('email')} value={email} sx={{m: 2, width: 300}} id="outlined-basic" label="Username" variant="outlined"/>
-                    <TextField onChange={handleChange('password')} value={password} sx={{m: 2, width: 300}} id="outlined-basic" label="Password" variant="outlined"/>
+                    <TextField onChange={handleChange('email')} value={email} sx={{m: 2, width: 300}}
+                               id="outlined-basic" label="Username" variant="outlined"/>
+                    <TextField onChange={handleChange('password')} value={password} sx={{m: 2, width: 300}}
+                               id="outlined-basic" label="Password" variant="outlined"/>
                     <Button onClick={clickSubmit} sx={{m: 3}} variant={"contained"} color={"success"}>Login</Button>
                     <Button href={'/signup'} sx={{textTransform: 'capitalize', mb: 2}}>Create a free account!</Button>
                 </Box>
