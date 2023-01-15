594dcde20b60113127af6ebbf43b9dcda3c212f6
Andrew <44222520+andrewjthomsen@users.noreply.github.com>
2023-01-15 00:04:44 -0800

    Finished building backend functionality for forgot password

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,40 +1,41 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
-const { sendEmailWithNodemailer } = require("../helpers/email");
- 
+const {sendEmailWithNodemailer} = require("../helpers/email");
+const {resetPasswordValidator} = require("../validators/auth");
+
 exports.signup = (req, res) => {
-  const { name, email, password } = req.body;
- 
-  User.findOne({ email }).exec((err, user) => {
-    if (user) {
-      return res.status(400).json({
-        error: "Email is taken",
-      });
-    }
- 
-    const token = jwt.sign(
-      { name, email, password },
-      process.env.JWT_ACCOUNT_ACTIVATION,
-      { expiresIn: "10m" }
-    );
- 
-    const emailData = {
-      from: "athomsen2639@gmail.com", // MAKE SURE THIS EMAIL IS YOUR GMAIL FOR WHICH YOU GENERATED APP PASSWORD
-      to: email, // WHO SHOULD BE RECEIVING THIS EMAIL? IT SHOULD BE THE USER EMAIL (VALID EMAIL ADDRESS) WHO IS TRYING TO SIGNUP
-      subject: "ACCOUNT ACTIVATION LINK",
-      html: `
+    const {name, email, password} = req.body;
+
+    User.findOne({email}).exec((err, user) => {
+        if (user) {
+            return res.status(400).json({
+                error: "Email is taken",
+            });
+        }
+
+        const token = jwt.sign(
+            {name, email, password},
+            process.env.JWT_ACCOUNT_ACTIVATION,
+            {expiresIn: "10m"}
+        );
+
+        const emailData = {
+            from: "athomsen2639@gmail.com", // MAKE SURE THIS EMAIL IS YOUR GMAIL FOR WHICH YOU GENERATED APP PASSWORD
+            to: email, // WHO SHOULD BE RECEIVING THIS EMAIL? IT SHOULD BE THE USER EMAIL (VALID EMAIL ADDRESS) WHO IS TRYING TO SIGNUP
+            subject: "ACCOUNT ACTIVATION LINK",
+            html: `
                 <h1>Please use the following link to activate your account</h1>
                 <p>http://localhost:3000/auth/activate/${token}</p>
                 <hr />
                 <p>This email may contain sensitive information</p>
                 <p>http://localhost:3000</p>
             `,
-    };
- 
-    sendEmailWithNodemailer(req, res, emailData);
- 
-  });
+        };
+
+        sendEmailWithNodemailer(req, res, emailData);
+
+    });
 };
 // // sendgrid
 // const sgMail = require('@sendgrid/mail');
@@ -83,10 +84,10 @@ exports.signup = (req, res) => {
 // };
 
 exports.accountActivation = (req, res) => {
-    const { token } = req.body;
+    const {token} = req.body;
 
     if (token) {
-        jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, function(err, decoded) {
+        jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, function (err, decoded) {
             if (err) {
                 console.log('JWT VERIFY IN ACCOUNT ACTIVATION ERROR', err);
                 return res.status(401).json({
@@ -94,9 +95,9 @@ exports.accountActivation = (req, res) => {
                 });
             }
 
-            const { name, email, password } = jwt.decode(token);
+            const {name, email, password} = jwt.decode(token);
 
-            const user = new User({ name, email, password });
+            const user = new User({name, email, password});
 
             user.save((err, user) => {
                 if (err) {
@@ -118,9 +119,9 @@ exports.accountActivation = (req, res) => {
 };
 
 exports.signin = (req, res) => {
-    const { email, password } = req.body;
+    const {email, password} = req.body;
     // check if user exist
-    User.findOne({ email }).exec((err, user) => {
+    User.findOne({email}).exec((err, user) => {
         if (err || !user) {
             return res.status(400).json({
                 error: 'User with that email does not exist. Please signup'
@@ -133,13 +134,53 @@ exports.signin = (req, res) => {
             });
         }
         // generate a token and send to client
-        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
-        const { _id, name, email, role } = user;
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+        const {_id, name, email, role} = user;
 
         return res.json({
             token,
-            user: { _id, name, email, role }
+            user: {_id, name, email, role}
         });
     });
 };
 
+// forgotPassword, resetPassword
+
+exports.forgotPassword = (req, res) => {
+    //
+    const {email} = req.body
+
+    User.findOne({email}, (err, user) => {
+        // if there isn't a user
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User with that email does not exist!'
+            });
+        }
+        // otherwise, generate token
+        const token = jwt.sign({ _id: user._id}, process.env.JWT_RESET_PASSWORD, {expiresIn: "10m"});
+
+        const emailData = {
+            from: "athomsen2639@gmail.com", // MAKE SURE THIS EMAIL IS YOUR GMAIL FOR WHICH YOU GENERATED APP PASSWORD
+            to: email, // WHO SHOULD BE RECEIVING THIS EMAIL? IT SHOULD BE THE USER EMAIL (VALID EMAIL ADDRESS) WHO IS TRYING TO SIGNUP
+            subject: "PASSWORD RESET LINK",
+            html: `
+                <h1>Please use the following link to reset your password</h1>
+                <p>http://localhost:3000/auth/password/reset/${token}</p>
+                <hr />
+                <p>This email may contain sensitive information</p>
+                <p>http://localhost:3000</p>
+            `,
+        };
+
+        sendEmailWithNodemailer(req, res, emailData);
+
+    });
+// }
+// })
+}
+
+exports.resetPassword = (req, res) => {
+    //
+
+}
